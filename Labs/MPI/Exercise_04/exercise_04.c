c6e8d1bce840f72a8420cc4e49c9809b61877c74
mantofer02 <mantofer2000@gmail.com>
2023-01-31 10:32:22 -0600

    work

--- Labs/MPI/Exercise_04/exercise_04.c
+++ Labs/MPI/Exercise_04/exercise_04.c
@@ -15,13 +15,13 @@ int main(int argc, char *argv[])
 
   int it = my_rank;
   a = (float)my_rank;
-  for (int i = 0; i < n_procs; i++)
-  {
-    MPI_Isend(&a, SIZE, MPI_FLOAT, (it + n_procs - 1) % n_procs, 0, MPI_COMM_WORLD, &request);
-    MPI_Irecv(&a, SIZE, MPI_FLOAT, (it + 1) % n_procs, 0, MPI_COMM_WORLD, &request);
-    // sum += b;
-    MPI_Wait(&request, MPI_STATUS_IGNORE);
-  }
+  // for (int i = 0; i < n_procs; i++)
+  // {
+  MPI_Isend(&a, SIZE, MPI_FLOAT, (it + n_procs - 1) % n_procs, 0, MPI_COMM_WORLD, &request);
+  MPI_Irecv(&a, SIZE, MPI_FLOAT, (it + 1) % n_procs, 0, MPI_COMM_WORLD, &request);
+  // sum += b;
+  MPI_Wait(&request, MPI_STATUS_IGNORE);
+  // }
 
   printf("I am process %d and sum = %f\n", my_rank, sum);
 
