6c52da7d0f0f3eedc7da811806993911fc552db8
mantofer02 <mantofer2000@gmail.com>
2023-01-30 23:35:26 -0600

    polit

--- MPI/Exercises/Exercise_02/exercise_02.c
+++ MPI/Exercises/Exercise_02/exercise_02.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
     MPI_Recv(data, SIZE, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     MPI_Send(buffer, SIZE, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
   }
-  printf("Process %d received data: %f\n", my_rank, buffer[4]);
+  printf("Process %d received data: %f\n", my_rank, data[4]);
 
   error = MPI_Finalize();
 }
