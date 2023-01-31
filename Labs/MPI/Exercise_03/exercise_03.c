23719e4f5cb5f975723351cae42ffdbd3ce7ee91
mantofer02 <mantofer2000@gmail.com>
2023-01-31 09:15:44 -0600

    working

--- Labs/MPI/Exercise_03/exercise_03.c
+++ Labs/MPI/Exercise_03/exercise_03.c
@@ -13,10 +13,12 @@ int main(int argc, char *argv[])
   MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-  memset(a, my_rank, sizeof(buffer));
+  memset(a, my_rank, sizeof(a));
 
-  MPI_Isend(a, SIZE, MPI_FLOAT, (my_rank + 1) % n_procs, 0, MPI_COMM_WORLD);
-  MPI_Irecv(b, SIZE, MPI_FLOAT, (my_rank - 1 >= 0 ? my_rank : n_procs - 1, 0, MPI_COMM_WORLD));
+  MPI_Request request;
+
+  MPI_Isend(a, SIZE, MPI_FLOAT, (my_rank + 1) % n_procs, 0, MPI_COMM_WORLD, &request);
+  MPI_Irecv(b, SIZE, MPI_FLOAT, (my_rank - 1 >= 0 ? my_rank : n_procs - 1), 0, MPI_COMM_WORLD, &request);
 
   printf("I am rocess %d  and I received value: %f\n", my_rank, b[5]);
 
