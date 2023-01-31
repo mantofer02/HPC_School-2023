c17960625fbb6c66a5f3a5e040ee6f5aa43f876e
luisbola12 <luibv1202@gmail.com>
2023-01-31 06:02:45 -0600

    Commit con implementacion de la funcion add_decorator

--- Proyecto/Version 1/model.cpp
+++ Proyecto/Version 1/model.cpp
@@ -16,3 +16,13 @@ std::tuple<std::vector<float>, std::vector<std::string>> Model::create_model() {
 
     return {transition, sequences};
 }
+std::vector<std::string> Model::add_decorator(std::vector<std::string> words, int num_decorators,char decorator){
+  for(int i = 0;i < words.size(); i++){
+        std::string decorator_str = "";
+        for(int j = 0; j < num_decorators;j++){
+            decorator_str += decorator; 
+        }
+        words[i] = decorator_str + words[i] + decorator_str;
+        std::transform(words[i].begin(), words[i].end(), words[i].begin(),[](unsigned char c){ return std::tolower(c); });
+    }
+};
--- Proyecto/Version 1/model.h
+++ Proyecto/Version 1/model.h
@@ -4,6 +4,7 @@
 #include <tuple>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 class Model {
   private:
@@ -12,6 +13,7 @@ class Model {
   public:
     Model(char);
     std::tuple<std::vector<float>, std::vector<std::string>> create_model();
+    std::vector<std::string> add_decorator(std::vector<std::string>,int num_decorators,char decorator);
 
 };
 
