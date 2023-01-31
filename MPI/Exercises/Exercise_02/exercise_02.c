fb2d3ce0e0c22f1be8b7277d805113e8ba3f2dcb
mantofer02 <mantofer2000@gmail.com>
2023-01-30 22:43:26 -0600

    mpi

--- MPI/Exercises/Exercise_02/exercise_02.c
+++ MPI/Exercises/Exercise_02/exercise_02.c
@@ -8,16 +8,16 @@ int main(int argc, char * argv[]) {
 
   MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-  printf("Hello, I am %d of %d processes \n", my_rank, n_procs);
 
-  float data[10000];
-  memset(data, my_rank, sizeof(data));
+  float buffer[10000];
+  float data[10000]
+  memset(buffer, my_rank, sizeof(data));
 
    if (my_rank == 0) {
-    MPI_Send(&data, 1, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
+    MPI_Send(&buffer, 1, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
     MPI_Recv(&data, 1, MPI_FLOAT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
   } else if (my_rank == 1) {
-    MPI_Send(&data, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
+    MPI_Send(&buffer, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
     MPI_Recv(&data, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
   }
     printf("Process %d received data: %f\n", my_rank, data[4]);
